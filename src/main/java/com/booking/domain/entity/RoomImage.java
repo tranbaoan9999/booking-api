305ab64fdf88db63f7216addb4b4f780325cf03c
An Tran <antranbao2@gmail.com>
2026-05-30 12:01:50 +0700

    Create room_images entity

---
+++ src/main/java/com/booking/domain/entity/RoomImage.java
@@ -0,0 +1,38 @@
+package com.booking.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "room_images")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class RoomImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+
+    @JoinColumn(name = "room_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Room room;
+
+    @Column(name = "is_thumbnail")
+    private boolean isThumbnail;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
